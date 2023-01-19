f4385ae5015449f3790690a82464546294e704f3
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2023-01-19 22:32:39 +0100

    Create Login.jsx component

--- my-app/src/components/App.jsx
+++ my-app/src/components/App.jsx
@@ -1,14 +1,32 @@
 import React from "react";
+import Login from "./Login";
+import Input from "./Input";
+
+var isLoggedIn=false;
+
+function renderConditionally(){
+    if(isLoggedIn===true){
+      return  (
+      <h1>Hello</h1>
+      );
+    }
+
+    else{
+        return(
+        <div>
+            <Login/>
+        </div>
+
+     
+        );
+    }
+}
 
 function App(){
 return(
     <div className="container">
-        <h1>Hello</h1>
-        <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Login</button>
-        </form>
+       {renderConditionally()}
+
     </div>
 );
 }
